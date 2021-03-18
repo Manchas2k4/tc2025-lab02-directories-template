d1b323a8d2f02358d4718802cd52e868a6f0b03a
Pedro Perez <pperezm@tec.mx>
2021-03-18 14:26:18 -0600

    test change: modifing the test file at the begining of the script

--- test.sh
+++ test.sh
@@ -12,10 +12,14 @@
 
 rm temp.txt run.txt app
 
+sed -i 's/\r//g' $1
+sed -i 's/\r//g' non_recursive.txt
+sed -i 's/\r//g' recursive.txt
+
 cal=0
 echo "Compiling..."
 gcc $1 -o app > /dev/null 2>&1
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -24,7 +28,7 @@ cal=$((cal+10))
 
 echo "Checking parameters..."
 ./app > /dev/null 2>&1
-if [[ $? -ne 255 ]] ; then 
+if [[ $? -ne 255 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -34,7 +38,7 @@ cal=$((cal+10))
 
 echo "Verifying that the directory exists..."
 ./app apt not_exist > /dev/null 2>&1
-if [[ $? -ne 254 ]] ; then 
+if [[ $? -ne 254 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -43,7 +47,7 @@ cal=$((cal+5))
 
 echo "Checking if it's a directory..."
 ./app apt test.sh > /dev/null 2>&1
-if [[ $? -ne 254 ]] ; then 
+if [[ $? -ne 254 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -52,7 +56,7 @@ cal=$((cal+5))
 
 echo "Verifying that the recursion parameter is valid (-r)..."
 ./app apt dir1 -x > /dev/null 2>&1
-if [[ $? -ne 253 ]] ; then 
+if [[ $? -ne 253 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -61,7 +65,7 @@ cal=$((cal+10))
 
 echo "Running non-recursive..."
 ./app apt dir1 > temp.txt
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -69,9 +73,8 @@ sed -i '/^$/d;s/[[:blank:]]//g' temp.txt
 sed -i 's/\r//g' temp.txt
 sort temp.txt > run.txt
 
-sed -i 's/\r//g' non_recursive.txt
 diff run.txt non_recursive.txt > /dev/null 2>&1
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -80,7 +83,7 @@ cal=$((cal+30))
 
 echo "Running recursive..."
 ./app apt dir1 -r > temp.txt
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
@@ -88,9 +91,8 @@ sed -i '/^$/d;s/[[:blank:]]//g' temp.txt
 sed -i 's/\r//g' temp.txt
 sort temp.txt > run.txt
 
-sed -i 's/\r//g' recursive.txt
 diff run.txt recursive.txt > /dev/null 2>&1
-if [[ $? -ne 0 ]] ; then 
+if [[ $? -ne 0 ]] ; then
 	echo "grade: $cal"
 	exit 1
 fi
